dd2af5804a4f13c7a58194465da98e2b0ede8d3a
frad00r4 <frad00r4@gmail.com>
2011-12-02 17:28:38 +0400

    fix test

--- test.c
+++ test.c
@@ -9,7 +9,7 @@
 
 int main() {
 	char string[] = "string|s:22:\\\"\\\\`1234567890-=+\\\"_\\\\/*a'\\\";digit|i:1317209150;boolean|b:1;array1|a:2:{i:1;s:3:\\\"qqq\\\";s:5:\\\"test2\\\";i:222;}array2|a:2:{i:0;s:3:\\\"qqq\\\";i:1;i:222;}";
-	session_t *test;
+	phpserialize_session_t *test;
 	char *res;
 	
 	printf( "%s\n", string );
